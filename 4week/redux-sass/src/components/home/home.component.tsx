2de8c007aaa7acc156b97d68745290d428ba6c65
Blake Kruppa <btkruppa513@gmail.com>
2018-09-17 13:34:13 -0400

    added some microservices

--- 4week/redux-sass/src/components/home/home.component.tsx
+++ 4week/redux-sass/src/components/home/home.component.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
-declare let CanvasDraw: any;
-import CanvasDraw from "react-canvas-draw";
 
 export class HomeComponent extends React.Component {
 
   public render() {
     return (
       <div>
-        <CanvasDraw />
+        home
       </div>
     );
   }
