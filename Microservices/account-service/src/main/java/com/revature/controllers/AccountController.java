8b6f5fb8a92a7346e28eb6341c554ec02718fe93
Blake Kruppa <btkruppa513@gmail.com>
2018-09-18 08:55:11 -0400

    pushed demos from yesterday

--- Microservices/account-service/src/main/java/com/revature/controllers/AccountController.java
+++ Microservices/account-service/src/main/java/com/revature/controllers/AccountController.java
@@ -6,21 +6,23 @@ import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.model.Account;
 
 @RestController
+@RequestMapping("accounts")
 public class AccountController {
 	private List<Account> accounts;
 
 	public AccountController() {
 		accounts = new ArrayList<>();
-		accounts.add(new Account(1, 3, "account 1", 50));
-		accounts.add(new Account(2, 1, "account 1", 500000));
-		accounts.add(new Account(3, 2, "account 1", 250));
-		accounts.add(new Account(4, 1, "account 1", 123540));
-		accounts.add(new Account(5, 1, "account 1", 2));
+		accounts.add(new Account(1, 3, "account 120", 50));
+		accounts.add(new Account(2, 1, "account 280", 500000));
+		accounts.add(new Account(3, 2, "account 33", 250));
+		accounts.add(new Account(4, 1, "account 14", 123540));
+		accounts.add(new Account(5, 1, "account 52", 2));
 	}
 
 	@GetMapping
---
+++ Microservices/customer-service/src/main/java/com/revature/controllers/CustomerController.java
@@ -0,0 +1,43 @@
+package com.revature.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.intercomm.AccountClient;
+import com.revature.model.Customer;
+
+@RestController
+@RequestMapping("customers")
+public class CustomerController {
+
+	@Autowired
+	private AccountClient ac;
+
+	private List<Customer> customers;
+
+	public CustomerController() {
+		customers = new ArrayList<>();
+		customers.add(new Customer(1, "Chris W.", null));
+		customers.add(new Customer(2, "John Brown", null));
+		customers.add(new Customer(3, "Austin", null));
+		customers.add(new Customer(4, "Stephen", null));
+		customers.add(new Customer(5, "Kelton", null));
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity<Customer> findById(@PathVariable int id) {
+		ResponseEntity<Customer> resp;
+		Customer c = customers.stream().filter(cust -> cust.getId() == id).findFirst().get();
+		c.setAccounts(ac.getAccountsByCustomer(c.getId()));
+		resp = new ResponseEntity<>(c, HttpStatus.OK);
+		return resp;
+	}
+}
