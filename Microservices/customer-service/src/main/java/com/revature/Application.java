c915468eec09887ac1d856caabe0584a82a77fb1
Blake Kruppa <btkruppa513@gmail.com>
2018-09-18 16:30:01 -0400

    added a circuit breaker for my feign client

--- Microservices/customer-service/src/main/java/com/revature/Application.java
+++ Microservices/customer-service/src/main/java/com/revature/Application.java
@@ -2,12 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
